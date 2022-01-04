1beb42b9e313ddaa559e1e62e04506c837c39f6d
unknown <lamica.olivares@gmail.com>
2022-01-04 10:07:24 -0300

    render first  component

--- src/App.js
+++ src/App.js
@@ -1,40 +1,10 @@
-/*import logo from './logo.svg';
-import './App.css';
 
-function App() {
-  return (  
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-          <p>hola amigas</p>
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+import { Home } from "./components/home";
 
-export default App;
-*/
-
-import React from "react";
-
-
-import { Home} from "./components/home";
-import { Order } from "./components/waitress/order";
 
 function App() {
   return (
-    <Router>
-      </Router>
-        );
-      }
+    <Home />
+  );
+}
+export default App;
--- src/components/home.jsx
+++ src/components/home.jsx
@@ -1,19 +1,14 @@
-import React from 'react';
+
 //import imagen
 export const Home = ()=> {
     return (
-        <section className="home">
+        <section style={{textAlign:"center"}}>
         <h1>Bienvenides al sistema de pedidos</h1>
         <p>Coffe Quuens</p>
-        <article>
-          <figure>
-            <img src={} />
-          </figure>
-          <ul>
+        <div>
            <button>Kitchen</button>
            <button>Waitress</button>
-          </ul>
-        </article>
+           </div>
       </section>
     )
   }
